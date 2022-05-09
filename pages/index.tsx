0d729f356e55a80b2c34b3651b23eb6e606689d9
minhtoan137 <duongminhtoan137@gmail.com>
2022-05-09 23:42:50 +0700

    change typescript index pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ interface HomeProps {
   documentaries: Movie[]
 }
 
-const Home: React.FC<HomeProps> = ({
+const Home = ({
   netflixOriginals,
   trendingNow,
   topRated,
@@ -26,7 +26,7 @@ const Home: React.FC<HomeProps> = ({
   horrorMovies,
   romanceMovies,
   documentaries
-}) => {
+}: HomeProps) => {
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
